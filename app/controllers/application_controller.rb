dc42128bfe2c6e754afa5f6795b45c094999a560
David <david.r.fankhauser@gmail.com>
2017-02-13 15:18:27 -0500

    new form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include_helper 
+  ##core controller of the application 
+
+  ##core of web request 
+
+  #fuck everything up
+
 end
